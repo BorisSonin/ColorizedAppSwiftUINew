526ca774f9c36126d675b391996c6b7117352dc1
BorisSonin <bsonin85@gmail.com>
2023-01-14 16:09:23 +0300

    Initial Commit

---
+++ ColorizedAppSwiftUINew/ColorizedAppSwiftUINewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSwiftUINewApp.swift
+//  ColorizedAppSwiftUINew
+//
+//  Created by Борис Сонин on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSwiftUINewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
